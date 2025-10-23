2bc546963224b7823d724a84df0db340aa91728e
王梓 <47105492+Bigge42@users.noreply.github.com>
2025-10-23 15:38:27 +0800

    fix: correct MaterialCallBoard namespaces

---
+++ api/HDPro.CY.Order/IRepositories/MaterialCallBoard/Partial/IMaterialCallBoardRepository.cs
@@ -0,0 +1,10 @@
+/*
+ *所有关于MaterialCallBoard类的自定义仓储接口应在此处编写
+*/
+
+namespace HDPro.CY.Order.IRepositories
+{
+    public partial interface IMaterialCallBoardRepository
+    {
+    }
+}
---
+++ api/HDPro.CY.Order/Repositories/MaterialCallBoard/Partial/MaterialCallBoardRepository.cs
@@ -0,0 +1,10 @@
+/*
+ *MaterialCallBoard仓储自定义扩展应在此处编写
+*/
+
+namespace HDPro.CY.Order.Repositories
+{
+    public partial class MaterialCallBoardRepository
+    {
+    }
+}
