76c17a58df431897d432b76e061a848ac83b0543
王梓 <47105492+Bigge42@users.noreply.github.com>
2025-10-23 15:38:27 +0800

    fix: correct MaterialCallBoard namespaces

---
+++ api/HDPro.CY.Order/IServices/MaterialCallBoard/Partial/IMaterialCallBoardService.cs
@@ -0,0 +1,15 @@
+/*
+ *所有关于MaterialCallBoard类的业务代码接口应在此处编写
+*/
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using HDPro.Core.Utilities;
+using HDPro.CY.Order.Models.MaterialCallBoardDtos;
+
+namespace HDPro.CY.Order.IServices
+{
+    public partial interface IMaterialCallBoardService
+    {
+        Task<WebResponseContent> BatchUpsertAsync(IEnumerable<MaterialCallBoardBatchDto> entries);
+    }
+}
