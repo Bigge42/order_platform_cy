2c9f3cc03b3b6c3c77d2f1e54891fa28a422e391
王梓 <582364398@qq.com>
2025-10-23 15:13:52 +0800

    重头开始

--- web.vite/src/views/order/materialcallboard/MaterialCallBoard/options.js
+++ web.vite/src/views/order/materialcallboard/MaterialCallBoard/options.js
@@ -21,6 +21,9 @@ export default function(){
     const searchFormOptions = [[{"title":"计划跟踪号","field":"PlanTrackNo"},{"title":"产品编号","field":"ProductCode"},{"title":"计划跟踪号","field":"CallerName"},{"title":"叫料时间","field":"CalledAt"}]];
     const columns = [{field:'WorkOrderNo',title:'WorkOrderNo',type:'string',width:110,hidden:true,readonly:true,require:true,align:'left'},
                        {field:'PlanTrackNo',title:'计划跟踪号',type:'string',sort:true,width:110,readonly:true,require:true,align:'left'},
+                       {field:'ProductCode',title:'产品编号',type:'string',sort:true,width:110,readonly:true,require:true,align:'left'},
+                       {field:'CallerName',title:'计划跟踪号',type:'string',sort:true,width:120,readonly:true,require:true,align:'left'},
+                       {field:'CalledAt',title:'叫料时间',type:'datetime',sort:true,width:110,readonly:true,require:true,align:'left'}];
     const detail ={columns:[]};
     const details = [];
 
