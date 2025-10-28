2fd79531f9608569a1a28f9906d3f27d73614d56
王梓 <582364398@qq.com>
2025-10-28 15:14:55 +0800

    wms同步接口更新

---
+++ api/HDPro.CY.Order/IServices/MaterialCallBoard/IMaterialCallWorkOrderSetService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HDPro.Core.Utilities;
+using HDPro.CY.Order.Models.MaterialCallBoardDtos;
+
+namespace HDPro.CY.Order.IServices.MaterialCallBoard
+{
+    public interface IMaterialCallWorkOrderSetService
+    {
+        /// <summary>全量快照写入并计算 ExistsInMCB</summary>
+        Task<WebResponseContent> RefreshSnapshotAsync(List<MaterialCallWorkOrderCodeDto> list);
+
+        /// <summary>按白名单删除叫料看板中“缺席”的记录</summary>
+        Task<WebResponseContent> PruneAsync();
+    }
+}
