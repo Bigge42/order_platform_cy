c90757c37a222bc8c30d23ee20ca785fa83cd9ed
王梓 <47105492+Bigge42@users.noreply.github.com>
2025-11-28 20:05:30 +0800

    Add valve rule batch inference integration

--- api/HDPro.CY.Order/IServices/WZ_OrderCycleBase/Partial/IWZ_OrderCycleBaseService.cs
+++ api/HDPro.CY.Order/IServices/WZ_OrderCycleBase/Partial/IWZ_OrderCycleBaseService.cs
@@ -5,9 +5,17 @@ using HDPro.Core.BaseProvider;
 using HDPro.Entity.DomainModels;
 using HDPro.Core.Utilities;
 using System.Linq.Expressions;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using HDPro.CY.Order.Models.OrderCycleBaseDtos;
 namespace HDPro.CY.Order.IServices
 {
     public partial interface IWZ_OrderCycleBaseService
     {
+        Task<int> SyncFromOrderTrackingAsync(CancellationToken cancellationToken = default);
+
+        Task<int> BatchCallValveRuleServiceAsync(List<OrderRuleDto> items, CancellationToken cancellationToken = default);
     }
  }
